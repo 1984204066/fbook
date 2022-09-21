6908cac7913163d77a72387aa0daa2f45f7879ab
1984204066 <1984204066@qq.com>
2022-09-21 14:32:01 +0800

    mv formal 虚构的古西方史.org to fake-west/org/0.org

--- history/fake-west/script/cmd.sh
+++ history/fake-west/script/cmd.sh
@@ -15,8 +15,17 @@ for i (*.org) {for str (${(f)"$(sed -n '/^(S)>[^><]*<(S)$/p' $i)"}) {echo "$#str
     
 for i ({1..26}) {read url;sd '###' "$url" ../org/$i.org;} <urls
     for i ({1..26}) {read url;sed -i '5s/.*/[[转载请 注明出处]['"$url"']]\n\n&/' ../org/$i.org;} <urls
-	
-    sed -i 'y/《》/「」/' *.org
+
+for i ({1..26}) {read url;echo $url;sed -i "3i$url\n\n" out/$i.org } <urls	
+
+i=0;for f (${(f)"$(sed -n "s,.*img src='file://\([^']\+\)'.*,\1,p" 0.org)"}){i=$((i+1)); f0=$f:t; e=$f:e; f1="0-$i.$e"; mv $f0 $f1; sed -i "s,file://\([^']\+\)'.*,./img/$f1," 0.org;}
+
+for f (img/0-*) {sed -i "1,\:./img/0-1.webp:s?./img/0-1.webp?/$f?" ../org/tmp.org }
+
+i=0;for f (${(f)"$(sed -n "s,.*img src='file://\([^']\+\)\('.*\),\2,p" 0.html)"}){i=$((i+1)); sd "'./img/0-$i.*$" "$0$f" 0.org;}
+
+sed -i 'y/《》/「」/' *.org
+
 # 黑体-> h2  跳过发言：之后3段。  
 sed -i '/:$/{n;n;n}; /^\*\**[^ :「]\+\*.*/s/\*\([^*]\+\)\*.*/** \1/' *.org
 for i ({1..26}) {echo -n "$i: "; sed -i -f ../modTitle.sed $i.org}
